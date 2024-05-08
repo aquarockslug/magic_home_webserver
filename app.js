832d295c74d085cf9d04afa1fe9b7c8a4f66ed94
aqua <aqua.rock.slug@gmail.com>
2024-05-08 12:05:53 +0000

    sysinfo request

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ var express = require('express')
 var app = express()
 app.use(express.static('public'))
 
+app.get('/white', (_, res) => sysinfo.fsSize('/dev/sda').then(data => res.send(data)))
+app.get('/sysinfo', async (_, res) => {
+        data = await Promise.all([sysinfo.fsSize(), sysinfo.cpu()])
+        res.send(data)
+})
+// light service
 var light = new Control("192.168.1.154")
-
 app.get('/on', (_, res) => light.setPower(true, () => res.end(light)))
 app.get('/off', (_, res) => light.setPower(false, () => res.end(light)))
-app.get('/white', (_, res) => sysinfo.fsSize().then(data => res.send(data)))
-app.post('/set', (req, res) => {
+app.post('/panel', (req, res) => {
         const b = req.body
         try {
                 if (b.power == 'on') light.turnOn()
                 if (b.power == 'off') light.turnOff()
                 if (b.r && b.g && b.b) light.setColor(b.r, b.g, b.b)
-        } catch (err) {
+        } catch (err) s{
                 console.log(err)
         } finally {
                 res.end('200')
