f89040c11ff3e2eb148216742034f8f8fd6936e9
aquarock <aqua.rock.slug@gmail.com>
2024-05-09 02:43:34 -0500

    style and more sysinfo

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ var sysinfo = require('systeminformation')
 var express = require('express')
 var app = express()
 app.use(express.static('public'))
+app.use(express.json());
 
 app.get('/white', (_, res) => sysinfo.fsSize('/dev/sda').then(data => res.send(data)))
-app.get('/sysinfo', async (_, res) => {
-        data = await Promise.all([sysinfo.fsSize(), sysinfo.cpu()])
-        res.send(data)
-})
+app.get('/sysinfo', async (_, res) => sysinfo.get({
+        osInfo: 'plaform, distro, node, npm',
+        system: 'model, manufacturer, raspberry',
+        currentLoad: 'avgLoad, currentLoadNice',
+        mem: 'total, used, active',
+        networkInterfaces: 'ip4, mac, speed',
+        bluetoothDevices: `device, name, manufacturer, macDevice, 
+			macHost, batteryPercent, type, connected`
+}).then(data => res.send(data)))
+
 // light service
-var light = new Control("192.168.1.154")
+var light = new Control("192.168.1.154", {})
 app.get('/on', (_, res) => light.setPower(true, () => res.end(light)))
 app.get('/off', (_, res) => light.setPower(false, () => res.end(light)))
 app.post('/panel', (req, res) => {
-        const b = req.body
+        const state = req.body // the requested state of the light panel
         try {
-                if (b.power == 'on') light.turnOn()
-                if (b.power == 'off') light.turnOff()
-                if (b.r && b.g && b.b) light.setColor(b.r, b.g, b.b)
+                light.setPower(state.on)
+                light.setColor(...state.color)
         } catch (err) {
                 console.log(err)
         } finally {
