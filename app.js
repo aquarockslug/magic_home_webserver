57d07cf3203a4dd26fa1af31cdfa3853ee4ca6e2
aquarock <aqua.rock.slug@gmail.com>
2024-05-10 08:36:09 -0500

    form

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var sysinfo = require('systeminformation')
 var express = require('express')
 var app = express()
 app.use(express.static('public'))
-app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+        extended: true
+}));
 
 app.get('/white', (_, res) => sysinfo.fsSize('/dev/sda').then(data => res.send(data)))
 app.get('/sysinfo', async (_, res) => sysinfo.get({
@@ -25,14 +28,18 @@ app.get('/off', (_, res) => light.setPower(false, () => res.end(light)))
 app.post('/panel', (req, res) => {
         const state = req.body // the requested state of the light panel
         try {
-                light.setPower(state.on)
-                light.setColor(...state.color)
+                light.setPower(true)
+                light.setColor(...readColorString(state.color))
         } catch (err) {
                 console.log(err)
         } finally {
-                res.end('200')
+                res.end(JSON.stringify(light.queryState()))
         }
 })
-
+var readColorString = (colorString) => [
+        Number(colorString.split(',')[0].slice(4)),
+        Number(colorString.split(',')[1].slice(1)),
+        Number(colorString.split(',')[2].slice(1, 2))
+]
 app.get('/', (_, res) => res.sendFile('index.html'))
 app.listen(8080, () => console.log('Server is listening on port 8080'))
