9e07a687859c028d024f0084bb8dbfdfb39d2772
aquarock <aqua.rock.slug@gmail.com>
2024-05-13 15:39:37 -0500

    color request

--- app.js
+++ app.js
@@ -28,31 +28,50 @@ app.get('/off', (_, res) => light.setPower(false, () => res.end('200')))
 app.post('/panel', async (req, res) => {
         const newState = req.body // the requested state of the light panel
         if (!newState) res.end('no request')
+        console.log('processing request...')
         try {
                 var panel = new Control("192.168.1.154", {})
-                await updateState(panel, newState)
-                console.log(await lightState(panel))
-                res.end('200')
+                await updateLightState(panel, newState)
+                console.log(await getLightState(panel))
         } catch (err) {
                 console.log(err)
         } finally {
-                console.log('processing request...')
+                res.redirect('/')
         }
 })
-var lightPowerPromise = (light, powerState) =>
-        new Promise((resolve, reject) => light.setPower(powerState, resolve))
-var updateState = async (light, newState) => {
-        var oldState = await lightState(light)
+var getLightState = async (light) => light.queryState().then((data) => data)
+var updateLightState = async (light, newState) => {
+        var oldState = await getLightState(light)
+        // update power state
+        var lightPowerPromise = (light, powerState) =>
+                new Promise((resolve, reject) =>
+                        light.setPower(powerState, resolve))
         oldState.on != ("on" == newState.on) ?
                 await lightPowerPromise(light, newState.on) :
                 console.log("no power state change");
-        // light.setColor(...readColorString(newState.color), () => 200)
+
+        // update color state
+        var lightColorPromise = (light, colorState) =>
+                new Promise((resolve, reject) =>
+                        light.setColor(...colorState, resolve))
+        var areColorsEqual = (a, b) =>
+                a.every((element, index) => element === b[index]);
+
+        var newColor = readColor(newState.color)
+        areColorsEqual(newColor, readColor(oldState.color)) ?
+                console.log("no color state change") :
+                await lightColorPromise(light, newColor)
 }
-var lightState = async (light) => light.queryState().then((data) => data)
-var readColorString = (colorString) => [
-        Number(colorString.split(',')[0].slice(4)),
-        Number(colorString.split(',')[1].slice(1)),
-        Number(colorString.split(',')[2].slice(1, 2))
-]
+var readColor = (color) => { // "rgb(0, 0, 0)" or {red, green, blue} -> [0, 0, 0]  
+        let readQueryColorObj = (color) => [color.red, color.green, color.blue]
+        let readColorPickerString = (colorString) => [
+                Number(colorString.split(',')[0].slice(4)),
+                Number(colorString.split(',')[1].slice(1)),
+                Number(colorString.split(',')[2].slice(1, -1))
+        ]
+        if (typeof color == 'string') return readColorPickerString(color)
+        if (typeof color == 'object') return readQueryColorObj(color)
+}
+
 app.get('/', (_, res) => res.sendFile('index.html'))
 app.listen(8080, () => console.log('Server is listening on port 8080'))
